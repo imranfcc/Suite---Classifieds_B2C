9838c7f046b8b89915bad54b3594d979217ba4ee
imranfcc <121158622+imranfcc@users.noreply.github.com>
2023-03-09 00:19:07 +0500

    update

--- page/item.php
+++ page/item.php
@@ -1,3 +1,16 @@
-<?php
-echo "comming soon";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php if(is_file("page/inc/HTML_HEAD.php")){ include("page/inc/HTML_HEAD.php"); } ?>
+</head>
+<body>
+<?php if(is_file("include/Pages/bodyHeader.cms.php")){ include("include/Pages/bodyHeader.cms.php"); } ?>
+<!-- Start : Your Page Source -->
+
+
+Comming soon
+
+<!-- End : Your Page Source -->
+<?php if(is_file("include/Pages/bodyBottom.cms.php")){ include("include/Pages/bodyBottom.cms.php"); } ?>
+</body>
+</html>

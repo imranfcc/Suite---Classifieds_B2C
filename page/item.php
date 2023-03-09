f4075f35bde83a8f85d668037a870b31118bfe91
Azhar Siddiqi <azhar@siddiquie.net>
2023-03-09 01:46:35 +0100

    Update item.php

--- page/item.php
+++ page/item.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
 	<?php if(is_file("page/inc/HTML_HEAD.php")){ include("page/inc/HTML_HEAD.php"); } ?>
 </head>
@@ -8,8 +8,11 @@
 <!-- Start : Your Page Source -->
 
 
+
+
+
 <?php 
-$sql = "SELECT * FROM `sh_items_info` WHERE `Id`='$path_exp[3]'";
+$sql = "SELECT * FROM `sh_items_info` WHERE `Id`='$path_exp[4]'";
 $result = $con->query($sql);
 if ($result->num_rows > 0) {
 	$row = $result->fetch_assoc();
@@ -17,8 +20,8 @@ if ($result->num_rows > 0) {
 	$Item_Description=$row["Description"];
 }
 
-if(isset($path_exp[4]) && $path_exp["4"]>"0"){
-	$sql = "SELECT * FROM `sh_sell_by_item` WHERE `Id`='$path_exp[4]'";
+if(isset($path_exp[5]) && $path_exp["5"]>"0"){
+	$sql = "SELECT * FROM `sh_sell_by_item` WHERE `Id`='$path_exp[5]'";
 	$result = $con->query($sql);
 	$seller_num=$result->num_rows;
 	if ($result->num_rows > 0) {
@@ -38,18 +41,17 @@ if(isset($path_exp[4]) && $path_exp["4"]>"0"){
 ?>
 
 
-
 <div class="mt-3" style="">
 	<div class="container" >
 		<div class="row" >
 			<div class="col-12 px-1" >
 				<div style="font-weight: bold; font-size:140%;"><?php echo $Item_Title; ?></div>
 				<div style="font-size:90%;">
-					Merk: <a href="<?php echo $path_exp["1"]."/b/".\P\Brands\brandID_to_Name($row["[sh_brand_names]Id"])."/".$row["[sh_brand_names]Id"]; ?>"><?php echo \P\Brand\brandID_to_Name($row["[sh_brand_names]Id"]); ?></a>
+					Brand: <a href="<?php echo $path_exp["1"]."/b/".\P\Brand\brandID_to_Name($row["[sh_pl_brand_names]Id"])."/".$row["[sh_pl_brand_names]Id"]; ?>"><?php echo \P\Brand\brandID_to_Name($row["[sh_pl_brand_names]Id"]); ?></a>
 					|
 					<i class="far fa-star"></i> <i class="far fa-star"></i> <i class="far fa-star"></i> <i class="far fa-star"></i> <i class="far fa-star"></i> review
 					|
-					<i class="fal fa-share-alt"></i> Delen
+					<i class="fal fa-share-alt"></i> Share
 				</div>
 			</div>
 		</div>
@@ -61,14 +63,14 @@ if(isset($path_exp[4]) && $path_exp["4"]>"0"){
 				<div class="rounded  p-1" style="border:1px solid #dee2e6;">
 					<?php if($row2["Quantity"]>"0"){  // Yes there is in stock ?>
 						<div class="font-weight-bold" style="font-size:160%;"><?php echo sh_amount($row2["Price"]); ?></div><br>
-						<?php if($row2["DeliveryTime"]=="1-8wd"){ ?><span class="text-success">1 - 2 weken</span><?php } ?>
-						<div class="mt-4">Verkoop door <a href="<?php echo $path_exp["1"]."/v/".suite\sellerID_to_Name($row2["[sh_sell_by]Id"])."/".$row2["[sh_sell_by]Id"];?>"><?php echo suite\sellerID_to_Name($row2["[sh_sell_by]Id"]); ?></a></div>
+						<?php if($row2["DeliveryTime"]=="1-8wd"){ ?><span class="text-success">1 - 2 weeks</span><?php } ?>
+						<div class="mt-4">Sold by <a href="<?php echo $path_exp["1"]."/v/".suite\sellerID_to_Name($row2["[sh_sell_by]Id"])."/".$row2["[sh_sell_by]Id"];?>"><?php echo suite\sellerID_to_Name($row2["[sh_sell_by]Id"]); ?></a></div>
 						<button type="button" class="btn btn-primary btn-master btn-lg btn-block mt-4" onclick="sh_load_url('<?php echo $path_exp['1']; ?>/item_AddToCart?AddItemId=<?php echo $itemInfo['Id']; ?>','#sh-Modal-Ajax');">In winkelwagen</button>
 						<div><i class="fas fa-check text-success"></i> <strong class="text-success">Gratis</strong> verzending</div>
 						<div><i class="fas fa-check text-success"></i> <strong class="text-success">30 dagen</strong> bedenktijd</div>
 					<?php } else { ?>
 						<div class="font-weight-bold text-danger" style="font-size:160%;">Niet leverbaar</div>
-						<div class="mt-4">Verkoop door <a href="<?php echo $path_exp["1"]."/v/".suite\sellerID_to_Name($row2["[sh_sell_by]Id"])."/".$row2["[sh_sell_by]Id"];?>"><?php echo suite\sellerID_to_Name($row2["[sh_sell_by]Id"]); ?></a></div>
+						<div class="mt-4">Sold by <a href="<?php echo $path_exp["1"]."/v/".suite\sellerID_to_Name($row2["[sh_sell_by]Id"])."/".$row2["[sh_sell_by]Id"];?>"><?php echo suite\sellerID_to_Name($row2["[sh_sell_by]Id"]); ?></a></div>
 						<button type="button" class="btn btn-primary btn-master btn-lg btn-block mt-4" onclick="sh_load_url('<?php echo $path_exp['1']; ?>/item_AddToCart?AddItemId=<?php echo $itemInfo['Id']; ?>','#sh-Modal-Ajax');" disabled>In winkelwagen</button>
 
 
@@ -86,6 +88,8 @@ if(isset($path_exp[4]) && $path_exp["4"]>"0"){
 		</div>
 	</div>
 </div>
+
+
 
 <!-- End : Your Page Source -->
 <?php if(is_file("include/Pages/bodyBottom.cms.php")){ include("include/Pages/bodyBottom.cms.php"); } ?>

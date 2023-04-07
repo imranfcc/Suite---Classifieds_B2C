f61a0e64e269d18a47aa92f2111e9b423a1f98b1
imranfcc <121158622+imranfcc@users.noreply.github.com>
2023-04-08 00:38:27 +0500

    Create cart.php

---
+++ page/cart.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php if(is_file("page/inc/HTML_HEAD.php")){ include("page/inc/HTML_HEAD.php"); } ?>
+</head>
+<body>
+<?php if(is_file("include/Pages/bodyHeader.cms.php")){ include("include/Pages/bodyHeader.cms.php"); } ?>
+<!-- Start : Your Page Source -->
+
+  this is a cart
+  
+<!-- End : Your Page Source -->
+<?php if(is_file("include/Pages/bodyBottom.cms.php")){ include("include/Pages/bodyBottom.cms.php"); } ?>
+</body>
+</html>
